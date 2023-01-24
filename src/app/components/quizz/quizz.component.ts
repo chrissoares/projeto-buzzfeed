23517bb02ccd87b5e584a56829f4ee58e006440a
chrissoares <chrisrsoares@gmail.com>
2023-01-23 23:48:12 -0300

    Entrega - Criando um Clone do BuzzFeed com Angular

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
@@ -6,10 +7,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quizz.component.css']
 })
 export class QuizzComponent implements OnInit {
+  title: string = ""
+  questions: any;
+  questionSelected: any;
+
+  answers: string[] = [];
+  answerSelected: string = "";
+
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  finished: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false;
+      this.title = quizz_questions.title;
+
+      this.questions = quizz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+      this.questionMaxIndex = this.questions.length;
+
+
+    }
   }
+
+  playerChoose(valor: string){
+    this.answers.push(valor);
+    this.nextStep();
+    console.log(this.answers);
+  }
+
+  async nextStep(){
+    this.questionIndex+=1;
+
+    if(this.questionMaxIndex > this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      const finalAnswer: string = await this.checkResult(this.answers)
+      this.finished = true;
+
+      this.answerSelected = quizz_questions.results[
+          //Faz o finalAnswer se comportar como uma chave (keyof) do
+          //tipo (typeof) do conteúdo de quizz_questions.results
+          finalAnswer as keyof typeof quizz_questions.results
+        ]
+    }
+  }
+
+  async checkResult(anwsers:string[]){
+    const result = anwsers.reduce((previous, current, i, arr) =>{
+      if (
+        arr.filter(item => item === previous).length >
+        arr.filter(item => item === current).length
+      ){
+        return previous;
+      } else {
+        return current;
+      }
+    })
+    return result;
+  }
+
 
 }
